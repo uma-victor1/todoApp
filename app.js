321d1e73e1dc1ca4986001af3a4699c3c52b41be
umavictor <umavictor11@gmail.com>
2020-07-27 18:06:10 +0100

    added slideshow

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const clear = document.querySelector(".clear");
-const date = document.querySelector("#date");
-const list = document.querySelector("#list");
-const items = document.getElementsByTagName("li");
-const text = document.querySelector(".text");
-const add_Todo = document.querySelector(".add-to-do i");
-const input = document.querySelector("input");
-let checked = "fa-check-circle";
-let unChecked = "fa-circle-thin";
-let line_through = "lineThrough";
-let LIST, id;
-let DATA = localStorage.getItem("TODO");
+var clear = document.querySelector(".clear");
+var date = document.querySelector("#date");
+var list = document.querySelector("#list");
+var header = document.querySelector(".header");
+var items = document.getElementsByTagName("li");
+var text = document.querySelector(".text");
+var add_Todo = document.querySelector(".add-to-do i");
+var input = document.querySelector("input");
+var checked = "fa-check-circle";
+var unChecked = "fa-circle-thin";
+var line_through = "lineThrough";
+var LIST = void 0,
+    id = void 0;
+var DATA = localStorage.getItem("TODO");
 if (DATA) {
   LIST = JSON.parse(DATA);
   id = LIST.length;
@@ -19,8 +21,23 @@ if (DATA) {
   id = 0;
 }
 
+//header
+
+var images = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg"];
+var index = 0;
+function imgSlide() {
+  header.style.backgroundImage = "url(" + ("../img/" + images[index]) + ")";
+  index++;
+  if (index == images.length) {
+    index = 0;
+  }
+  setTimeout(imgSlide, 6000);
+}
+
+imgSlide();
+
 function loadList(array) {
-  array.forEach((item) => {
+  array.forEach(function (item) {
     addTodo(item.name, item.id, item.done, item.trash);
   });
 }
@@ -28,37 +45,39 @@ function loadList(array) {
 function addTodo(toDo, id, done, trash) {
   if (trash) return;
 
-  let Done = done ? checked : unChecked;
-  let Line = done ? line_through : "";
+  var Done = done ? checked : unChecked;
+  var Line = done ? line_through : "";
 
-  const text = `<li class="item">
-  <i class="fa ${Done}
-   co" job = "complete" id = "${id}"></i>
-  <p class="text ${Line}">${toDo}</p>
-  <i class="fa fa-trash-o de" job = "delete" id = "${id}"></i>
-  </li>`;
-  const position = "beforeend";
+  var text = "<li class=\"item\">\n  <i class=\"fa " + Done + "\n   co\" job = \"complete\" id = \"" + id + "\"></i>\n  <p class=\"text " + Line + "\">" + toDo + "</p>\n  <i class=\"fa fa-trash-o de\" job = \"delete\" id = \"" + id + "\"></i>\n  </li>";
+  var position = "beforeend";
   list.insertAdjacentHTML(position, text);
+}
+function TODO() {
+  var toDo = input.value;
+  if (toDo) {
+    addTodo(toDo, id, false, false);
+    LIST.push({
+      name: toDo,
+      id: id,
+      done: false,
+      trash: false
+    });
+
+    input.value = "";
+    localStorage.setItem("TODO", JSON.stringify(LIST));
+    id++;
+  }
 }
 
 function doME(event) {
-  const toDo = input.value;
   if (event.keyCode == 13) {
-    if (toDo) {
-      addTodo(toDo, id, false, false);
-      LIST.push({
-        name: toDo,
-        id: id,
-        done: false,
-        trash: false,
-      });
-
-      input.value = "";
-      localStorage.setItem("TODO", JSON.stringify(LIST));
-      id++;
-    }
+    TODO();
   }
 }
+function doMeOnClick() {
+  TODO();
+}
+
 //complete todo
 function completeTodo(element) {
   element.classList.toggle(checked);
@@ -73,8 +92,8 @@ function deleteTodo(element) {
   LIST[element.id].trash = true;
 }
 function checkTodo(event) {
-  const element = event.target;
-  const elementJob = element.attributes.job.value;
+  var element = event.target;
+  var elementJob = element.attributes.job.value;
   if (elementJob == "complete") {
     completeTodo(element);
   } else if (elementJob == "delete") {
@@ -82,14 +101,19 @@ function checkTodo(event) {
   }
   localStorage.setItem("TODO", JSON.stringify(LIST));
 }
+
 document.addEventListener("keyup", doME);
+add_Todo.addEventListener("click", doMeOnClick);
 list.addEventListener("click", checkTodo);
 
+function pop() {
+  alert("working");
+}
 clear.addEventListener("click", function () {
   localStorage.clear();
   location.reload();
 });
 
-const today = new Date();
-let options = { weekday: "long", month: "short", day: "numeric" };
+var today = new Date();
+var options = { weekday: "long", month: "short", day: "numeric" };
 date.innerHTML = today.toLocaleDateString("en-US", options);
